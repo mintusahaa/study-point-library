6ccf573751ef4c2ad4484c82f374dad02159f387
mintusahaa <mintukumarsaw1@gmail.com>
2026-09-12 12:09:09 +0530

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// Add your Supabase project values here after creating the project.
-// IMPORTANT: use the publishable/anon key only. Never put a service-role secret in this file.
-window.SPL_CONFIG={SUPABASE_URL:'https://npzsahbolwrinceaoxmp.supabase.co',SUPABASE_KEY:'sb_publishable_zRF2_UWUAPAuLerwNWADgA_e_La72up'};
